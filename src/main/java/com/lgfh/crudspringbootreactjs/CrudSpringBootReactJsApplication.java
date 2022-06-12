f86c9f03a067b00743b9af363502f60e0be4d4d4
lgfh98 <lgfigueroah@outlook.com>
2022-06-12 08:32:42 -0500

    first commit

---
+++ src/main/java/com/lgfh/crudspringbootreactjs/CrudSpringBootReactJsApplication.java
@@ -0,0 +1,13 @@
+package com.lgfh.crudspringbootreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootReactJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringBootReactJsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgfh/crudspringbootreactjs/CrudSpringBootReactJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfh.crudspringbootreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootReactJsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
